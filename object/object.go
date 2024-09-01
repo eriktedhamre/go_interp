da7c8861bb79feff7e376600ffc264fd3cc1c546
Erik <42960003+eriktedhamre@users.noreply.github.com>
2024-09-01 14:48:59 +0200

    evaluating array index expressions

--- object/object.go
+++ object/object.go
@@ -18,7 +18,13 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
 
 type Function struct {
 	Parameters []*ast.Identifier
@@ -44,9 +50,24 @@ func (f *Function) Inspect() string {
 	return out.String()
 }
 
-type Object interface {
-	Type() ObjectType
-	Inspect() string
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
 }
 
 type ReturnValue struct {
